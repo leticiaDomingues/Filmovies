824a4127ca0568db280a7a485ad0fbeb4b720849
Letícia Domingues <leticia.domingues@opus-software.com.br>
2018-02-22 15:19:57 -0300

    Creating project structure (folders & libs)

---
+++ js/controllers/app.js
@@ -0,0 +1,13 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('app')
+		.controller('AppController', appController);
+
+	appController.$inject = ['$scope'];
+
+	function appController($scope) {
+		
+	}
+})();
