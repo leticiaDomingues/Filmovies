a2e3952559812669db7f6df6433a13cf158278d7
Letícia Domingues <leticia.domingues@hotmail.com>
2018-02-26 08:17:05 -0300

    Home page - structure

---
+++ js/app.routes.js
@@ -0,0 +1,16 @@
+(function() {
+	'use strict';
+	angular
+		.module('app')
+		.config(config);
+
+	config.$inject = ['$routeProvider'];
+
+	function config($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl : 'view/movies.html'
+			})
+			.otherwise({redirectTo:'/'});
+	}
+})();
---
+++ js/controllers/movies.js
@@ -0,0 +1,15 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('app')
+		.controller('MoviesController', moviesController);
+
+	moviesController.$inject = ['$scope'];
+
+	function moviesController($scope) {
+		var self = this;
+
+		
+	}
+})();
